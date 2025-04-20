b7d2884542f3e86374e4cdd01d63319d915728c4
2211846103 <m.ibrahim9276@gmail.com>
2025-04-20 18:38:07 +0200

    Turned Resource Manager into Singlton

--- include/resource_management/resource_manager.hpp
+++ include/resource_management/resource_manager.hpp
@@ -1,28 +1,33 @@
 #pragma once
 
+#include "core/types.hpp"
 #include <memory>
 #include <resource_management/resource_storage.hpp>
 
 
 namespace engine::resource_management {
 
+class ResourceManagerConfig {
+public:
+  bool hot_reload = false;
+  core::GraphicsAPI api = core::GraphicsAPI::OpenGL;
+};
+
 class ResourceManager {
 private:
-  bool _hot_reload;
-  FileStorage _file_storage;
-  ShaderStorage _shader_storage;
+  static FileStorage _file_storage;
+  static ShaderStorage _shader_storage;
 
 public:
-  ResourceManager(bool hot_reload = false);
-  ~ResourceManager() = default;
+  static ResourceManagerConfig config;
 
-  void setGraphicsLoader(core::GraphicsAPI api);
+  static void setGraphicsLoader(core::GraphicsAPI api);
 
-  std::shared_ptr<File> getFile(std::string name);
-  std::shared_ptr<Shader> getShader(std::string name);
+  static std::shared_ptr<File> getFile(std::string name);
+  static std::shared_ptr<Shader> getShader(std::string name);
 
-  std::shared_ptr<File> loadFile(std::string path, std::string name = "", bool lazy = true);
-  std::shared_ptr<Shader> loadShader(std::string vertex_path, std::string fragment_path, std::string name = "", bool lazy = true);
+  static std::shared_ptr<File> loadFile(std::string path, std::string name = "", bool lazy = true);
+  static std::shared_ptr<Shader> loadShader(std::string vertex_path, std::string fragment_path, std::string name = "", bool lazy = true);
 };
 
 }
--- src/resource_management/resource_manager.cpp
+++ src/resource_management/resource_manager.cpp
@@ -4,7 +4,10 @@
 
 using namespace engine::resource_management;
 
-ResourceManager::ResourceManager(bool hot_reload) : _hot_reload(hot_reload) {}
+ResourceManagerConfig ResourceManager::config = ResourceManagerConfig();
+
+FileStorage ResourceManager::_file_storage = FileStorage();
+ShaderStorage ResourceManager::_shader_storage = ShaderStorage();
 
 void ResourceManager::setGraphicsLoader(core::GraphicsAPI api) {
   FactoryManager::setGraphicsAPI(api);
@@ -18,11 +21,11 @@ std::shared_ptr<Shader> ResourceManager::getShader(std::string name) {
 }
 
 std::shared_ptr<File> ResourceManager::loadFile(std::string path, std::string name, bool lazy) {
-  return _file_storage.load(path, name, this->_hot_reload, lazy);
+  return _file_storage.load(path, name, ResourceManager::config.hot_reload, lazy);
 }
 std::shared_ptr<Shader> ResourceManager::loadShader(std::string vertex_path, std::string fragment_path, std::string name, bool lazy) {
-  std::shared_ptr<File> vertex_file = this->loadFile(vertex_path, "", lazy);
-  std::shared_ptr<File> fragment_file = this->loadFile(fragment_path, "", lazy);
+  std::shared_ptr<File> vertex_file = ResourceManager::loadFile(vertex_path, "", lazy);
+  std::shared_ptr<File> fragment_file = ResourceManager::loadFile(fragment_path, "", lazy);
 
   return _shader_storage.load(vertex_file, fragment_file, name, lazy);
 }

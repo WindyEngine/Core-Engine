b13a1932c2ef293f093c158da51faa9904d7c0c0
Sajed Tantush <SajedTantush@gmail.com>
2025-05-18 16:44:59 +0200

    seperated window interface from window manager

--- include/platform/window.hpp
+++ include/platform/window.hpp
@@ -1,6 +1,11 @@
 #pragma once
 
-#include <core/graphics_common.hpp>
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
+
+#include <d3d11.h>
+#pragma comment(lib, "d3d11.lib")
+
 #include <vector>
 
 namespace engine::platform {
@@ -9,13 +14,100 @@ namespace engine::platform {
         public:
             virtual ~Window() = default;
 
+            // Window operations
             virtual void initWindow() = 0;
-            virtual bool shouldClose() = 0;
-            virtual float update() = 0;
-            virtual void clear() = 0;
-            virtual void show() = 0;
+            virtual void showWindow() = 0;
+            virtual void hideWindow() = 0;
+            virtual void closeWindow() = 0;
+            virtual bool shouldClose() const = 0;
+
+            // Properties functions
+            virtual void setName(const char* name) = 0;
+            virtual char* getName() const = 0;
+            virtual std::pair<int, int> getSize() const = 0;
+            virtual void resize(int width, int height) = 0;
+
+            //
+            virtual void* getWindow() const = 0;
+
+    };
+    class GLFWWindow : public Window {
+        public:
+            GLFWWindow(int width, int height, const char* name);
+            ~GLFWWindow() override;
+
+            void initWindow() override;
+            void showWindow() override;
+            void hideWindow() override;
+            void closeWindow() override;
+            bool shouldClose() const override;
+
+            void setName(const char* name) override;
+            char* getName() const override;
+            std::pair<int, int> getSize() const override;
+            void resize(int width, int height) override;
+
+            void* getWindow() const override;
+
+        private:
+            GLFWwindow* _glfwWindow;
+            int _width;
+            int _height;
+            const char* _name;
     };
 
+    class Win32Window : public Window {
+        public:
+            Win32Window(int width, int height, const char* name);
+            ~Win32Window() override;
+
+            void initWindow() override;
+            void showWindow() override;
+            void hideWindow() override;
+            void closeWindow() override;
+            bool shouldClose() const override;
+
+            void setName(const char* name) override;
+            char* getName() const override;
+            std::pair<int, int> getSize() const override;
+            void resize(int width, int height) override;
+
+            void* getWindow() const override;
+
+        private:
+            int _width;
+            int _height;
+            const char* _name;
+
+            HWND _hwnd;
+            HINSTANCE _hInstance;
+    };
+
+    class CocoaWindow : public Window {
+        public:
+            CocoaWindow(int width, int height, const char* name);
+            ~CocoaWindow() override;
+
+            void initWindow() override;
+            void showWindow() override;
+            void hideWindow() override;
+            void closeWindow() override;
+            bool shouldClose() const override;
+
+            void setName(const char* name) override;
+            char* getName() const override;
+            std::pair<int, int> getSize() const override;
+            void resize(int width, int height) override;
+
+            void* getWindow() const override;
+
+        private:
+            GLFWwindow* glfwWindow; // placeholder change to Cocoa window
+            int _width;
+            int _height;
+            const char* _name;
+    };
+/*
 #ifdef ENGINE_COMPILE_OPENGL
     class OpenGLWindow : public Window {
         public:
@@ -127,4 +219,5 @@ namespace engine::platform {
             HINSTANCE _hInstance;
     };
 #endif
+*/
 }
--- include/platform/window_manager.hpp
+++ include/platform/window_manager.hpp
@@ -1,11 +1,10 @@
 #pragma once
 
+#include <platform/window.hpp>
+
 #include <memory>
 #include <unordered_map>
-#include <glad/glad.h>
-#include <glfw/glfw3.h>
 
-#include <core/graphics_common.hpp>
 #include <string>
 #include <stdexcept>
 
@@ -15,104 +14,6 @@ namespace engine::platform {
         Win32,
         Cocoa
     };
-
-    class Window {
-        public:
-            virtual ~Window() = default;
-
-            // Window operations
-            virtual void initWindow() = 0;
-            virtual void showWindow() = 0;
-            virtual void hideWindow() = 0;
-            virtual void closeWindow() = 0;
-            virtual bool shouldClose() const = 0;
-
-            // Properties functions
-            virtual void setName(const char* name) = 0;
-            virtual char* getName() const = 0;
-            virtual std::pair<int, int> getSize() const = 0;
-            virtual void resize(int width, int height) = 0;
-
-            //
-            virtual void* getWindow() const = 0;
-
-    };
-
-    class GLFWWindow : public Window {
-        public:
-            GLFWWindow(int width, int height, const char* name);
-            ~GLFWWindow() override;
-
-            void initWindow() override;
-            void showWindow() override;
-            void hideWindow() override;
-            void closeWindow() override;
-            bool shouldClose() const override;
-
-            void setName(const char* name) override;
-            char* getName() const override;
-            std::pair<int, int> getSize() const override;
-            void resize(int width, int height) override;
-
-            void* getWindow() const override;
-
-        private:
-            GLFWwindow* _glfwWindow;
-            int _width;
-            int _height;
-            const char* _name;
-    };
-
-    class Win32Window : public Window {
-        public:
-            Win32Window(int width, int height, const char* name);
-            ~Win32Window() override;
-
-            void initWindow() override;
-            void showWindow() override;
-            void hideWindow() override;
-            void closeWindow() override;
-            bool shouldClose() const override;
-
-            void setName(const char* name) override;
-            char* getName() const override;
-            std::pair<int, int> getSize() const override;
-            void resize(int width, int height) override;
-
-            void* getWindow() const override;
-
-        private:
-            GLFWwindow* glfwWindow; // placeholder change to win32 window
-            int _width;
-            int _height;
-            const char* _name;
-    };
-
-    class CocoaWindow : public Window {
-        public:
-            CocoaWindow(int width, int height, const char* name);
-            ~CocoaWindow() override;
-
-            void initWindow() override;
-            void showWindow() override;
-            void hideWindow() override;
-            void closeWindow() override;
-            bool shouldClose() const override;
-
-            void setName(const char* name) override;
-            char* getName() const override;
-            std::pair<int, int> getSize() const override;
-            void resize(int width, int height) override;
-
-            void* getWindow() const override;
-
-        private:
-            GLFWwindow* glfwWindow; // placeholder change to Cocoa window
-            int _width;
-            int _height;
-            const char* _name;
-    };
-
     class WindowManager {
         public:
             static std::shared_ptr<WindowManager> getInstance();

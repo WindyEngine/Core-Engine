c020e98084feca401d4ae52ffac88548bfb74021
2211846103 <m.ibrahim9276@gmail.com>
2025-07-15 18:27:14 +0200

    Re-Added Deleted CLI Logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,22 @@
-#include <platform/window_manager.hpp>
+//#include <platform/window_manager.hpp>
 #include <iostream>
+#include <CLI11/CLI11.hpp>
 
-using namespace engine::platform;
+
+//using namespace engine::platform;
 
 int main(int argc, char** argv) {
-  std::shared_ptr<WindowManager> windowManager = WindowManager::getInstance();
-  windowManager->createWindow(800, 600, "DXWindow", WindowAPI::Win32);
-  auto window = windowManager->getWindow("DXWindow");
-  window->showWindow();
+  // CLI Logic
+  // Whoever deletes this imma send you a dick pic
+  // Just comment it if dont need it
+  CLI::App cli;
+  argv = cli.ensure_utf8(argv);
+  std::string assetPath;
+  cli.add_option("-p, --project", assetPath, "Path to Project Folder")->required();
+  CLI11_PARSE(cli, argc, argv);
+
+  //std::shared_ptr<WindowManager> windowManager = WindowManager::getInstance();
+  //windowManager->createWindow(800, 600, "DXWindow", WindowAPI::Win32);
+  //auto window = windowManager->getWindow("DXWindow");
+  //window->showWindow();
 }

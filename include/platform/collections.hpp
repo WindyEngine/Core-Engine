48377fff2c4e6fd7cf9c2e54584605adb1b203fb
MIbrahim9276 <m.ibrahim9276@gmail.com>
2026-04-17 23:00:52 +0200

    docs(platform): add brief for collections

--- include/platform/collections.hpp
+++ include/platform/collections.hpp
@@ -1,6 +1,14 @@
+/** 
+ * @file collections.hpp
+ * @brief Defines core data structures used across the engine.
+*/
+
 #pragma once
 
+/**
+ * @namespace Windy::Platform
+ * @brief Contains low-level hardware abstraction and platform-specific logic
+ */
+namespace Windy::Platform {
 
-namespace Windy::Collections {
-    
 }
